54f1af4b336d0f1c555194e3025d23834317271b
Ferdroid8080 <andresf@ferudinato.com>
2020-12-20 14:43:34 -0500

    added some projects

--- initialState.js
+++ initialState.js
@@ -3,35 +3,39 @@ export default {
   projects: [
     {
       id: 100,
-      imageUrl: '',
-      projectUrl: '',
-      title: 'Project 01',
-      label: 'project-01',
-      description: 'etc etc...'
+      imageUrl: 'https://andresfmj-portfolio-images.s3.amazonaws.com/project-03.png',
+      projectUrl: 'https://github.com/andresfmj/fixCar',
+      websiteUrl: 'http://fixcars.ferdinania.com',
+      title: 'Fix Cars',
+      label: 'fix-cars',
+      description: 'Sistema de control de autos para talleres de reparacion'
     },
     {
       id: 101,
-      imageUrl: '',
-      projectUrl: '',
-      title: 'Project 02',
-      label: 'project-02',
-      description: 'another etc etc... etc'
+      imageUrl: 'https://andresfmj-portfolio-images.s3.amazonaws.com/project-04.png',
+      projectUrl: 'https://github.com/andresfmj/HuellaCarboC',
+      websiteUrl: 'http://huellacarbo.ferdinania.com/travels',
+      title: 'Huella Carbono',
+      label: 'huella-carbono',
+      description: 'Registro de huella de carbono dependiendo del medio de transporte usado por los trabajadores'
     },
     {
       id: 102,
-      imageUrl: '',
-      projectUrl: '',
-      title: 'Project 03',
-      label: 'project-03',
-      description: 'lorem ipsum lalalla bla bla'
+      imageUrl: 'https://andresfmj-portfolio-images.s3.amazonaws.com/project-100.png',
+      projectUrl: 'https://github.com/andresfmj/stormwind-database',
+      websiteUrl: 'https://priceless-aryabhata-d44d29.netlify.app',
+      title: 'Stormwind Database',
+      label: 'stormwind-database',
+      description: 'Buscador de objetos del popular juego World of Warcraft. Este cliente hecho en React se conecta a la API de Battle.net para obtener la data correspondiente en JSON.'
     },
     {
       id: 103,
-      imageUrl: '',
-      projectUrl: '',
-      title: 'Project 04',
-      label: 'project-04',
-      description: 'blabla bla bla hello there'
-    },
+      imageUrl: 'https://andresfmj-portfolio-images.s3.amazonaws.com/project-101.png',
+      projectUrl: 'https://github.com/andresfmj/social-network-react',
+      websiteUrl: 'https://wizardly-hamilton-9b427e.netlify.app/',
+      title: 'Social Network React.js',
+      label: 'social-network-react-js',
+      description: 'Este es un ejemplo de red social hecho en React utilizando DummyAPI para obtener posts de mascotas.'
+    }
   ]
 };
