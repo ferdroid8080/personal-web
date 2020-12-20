b223ab29c208828753511d9fe8dd35f2f8972e2d
Ferdroid8080 <andresf@ferudinato.com>
2020-12-20 17:20:47 -0500

    fixed some styles

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -4,17 +4,25 @@ import './Card.css';
 
 function Card({ item }) {
   return (
-    <a href={item.websiteUrl} title={item.label} className="Card">
+    <div className="Card">
       <div className="Card--Inner--Image">
         <img src={item.imageUrl} alt={item.title} width="450" />
       </div>
       <div className="Card--Header">
         <h2>{item.title}</h2>
+        <ul className="Card--Icons--Actions">
+          <li>
+            <a href={item.projectUrl} title={item.projectUrl}>github repo</a>
+          </li>
+          <li>
+            <a href={item.websiteUrl} title={item.websiteUrl}>live demo</a>
+          </li>
+        </ul>
       </div>
       <div className="Card--Body text-center">
         <p>{item.description}</p>
       </div>
-    </a>
+    </div>
   );
 }
 
