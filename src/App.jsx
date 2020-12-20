e6f4e22224f8b54a88c5a4c94954a4da8720c924
Ferdroid8080 <andresf@ferudinato.com>
2020-12-20 13:11:18 -0500

    added app context and initialState as provider

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,27 @@
 import React from 'react';
 
+import './App.css';
+
+import Projects from './containers/Projects';
+
+import AppContext from './context/AppContext';
+import useInitialState from './hooks/useInitialState';
 
 function App() 
 {
+  const initialState = useInitialState();
   return (
-    <h1>My Projects</h1>
+    <AppContext.Provider value={initialState}>
+      <div className='App'>
+        <hgroup className='text-center'>
+          <h1>My Projects</h1>
+          <h4>
+            <a href="https://github.com/andresfmj">github</a>
+          </h4>
+        </hgroup>
+        <Projects projects={initialState.projects} />
+      </div>
+    </AppContext.Provider>
   )
 }
 
