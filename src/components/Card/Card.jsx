8613bcaca007ea7c6031d82186dde4a933fb6cc2
Ferdroid8080 <andresf@ferudinato.com>
2020-12-20 13:47:50 -0500

    fixing page design

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -2,23 +2,20 @@ import React from 'react';
 
 import './Card.css';
 
-function Card({ item })
-{
+function Card({ item }) {
   return (
-    <a href={item.projectUrl} title={item.label}>
-      <div className="Card">
-        <div className="Card--Inner--Image">
-          <img src={item.imageUrl} alt={item.title} />
-        </div>
-        <div className="Card--Header">
-          <h2>{item.title}</h2>
-        </div>
-        <div className="Card--Body">
-          <p>{item.description}</p>
-        </div>
+    <a href={item.websiteUrl} title={item.label} className="Card">
+      <div className="Card--Inner--Image">
+        <img src={item.imageUrl} alt={item.title} width="450" />
+      </div>
+      <div className="Card--Header">
+        <h2>{item.title}</h2>
+      </div>
+      <div className="Card--Body text-center">
+        <p>{item.description}</p>
       </div>
     </a>
-  )
+  );
 }
 
 export default Card;
